2ed5e7ec3daa4991cf7a9f5e3d234b98f382b152
Glenn Hartmann <hartmann.glenn@gmail.com>
2026-08-18 16:26:33 -0400

    Fix nix.

--- flake.nix
+++ flake.nix
@@ -28,7 +28,21 @@
             lockFile = ./cli/Cargo.lock;
 
             outputHashes = {
-              "oauth2-0.0.2" = "sha256-ltguj0sgOmeCXws+JIaVI5+f4rk1IVZKSCoAX/acN74=";
+              "oauth2-0.0.3" = "sha256-4aap4FwENrIZdx8mn1olipljbpbv3FnL4hi90eBn2Dw=";
+            };
+          };
+        };
+        simple-manifest = (pkgs.lib.importTOML ./simple/Cargo.toml).package;
+        oauth2-simple = pkgs.rustPlatform.buildRustPackage {
+          pname = simple-manifest.name;
+          version = simple-manifest.version;
+          src = builtins.path { path = ./simple; name = "oauth2-simple"; };
+
+          cargoLock = {
+            lockFile = ./simple/Cargo.lock;
+
+            outputHashes = {
+              "oauth2-0.0.3" = "sha256-4aap4FwENrIZdx8mn1olipljbpbv3FnL4hi90eBn2Dw=";
             };
           };
         };
@@ -37,12 +51,14 @@
           paths = [
             oauth2
             oauth2-cli
+            oauth2-simple
           ];
         };
         oauth2-shell = pkgs.mkShell {
           inputsFrom = [
             oauth2
             oauth2-cli
+            oauth2-simple
           ];
           packages = with pkgs; [
             clippy
@@ -52,8 +68,8 @@
       in
       {
         packages = {
-          inherit oauth2 oauth2-cli all;
-          default = oauth2;
+          inherit oauth2 oauth2-cli oauth2-simple all;
+          default = all;
         };
         devShells = {
           inherit oauth2-shell;
