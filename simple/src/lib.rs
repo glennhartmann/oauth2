62e5dd93c3e074380e6e450160a5cae239c1d96e
Glenn Hartmann <hartmann.glenn@gmail.com>
2026-08-29 09:34:58 -0400

    Support DPoP in `simple` library.

--- simple/src/lib.rs
+++ simple/src/lib.rs
@@ -7,7 +7,10 @@ use std::{
     time::Duration,
 };
 
-use oauth2::{ClientInfo, Oauth2, Tokens};
+use oauth2::{
+    ClientInfo, Oauth2, RefreshHandleResponseError, Tokens,
+    dpop::{KeyData, Keypair, KeypairPemPkcs8},
+};
 
 use bon::Builder;
 
@@ -31,6 +34,21 @@ pub struct Cfg<'a> {
 
     /// OAuth2 API scope to authorize.
     pub scope: &'a str,
+
+    /// Paths to public and private keys for use with DPoPs. If a files don't already exist at
+    /// these paths, public and private keys will be generated and written there. DPoPs will
+    /// automatically be used if this is specified.
+    pub key_paths: Option<KeyPaths>,
+}
+
+/// Paths to public and private keys.
+#[derive(Clone)]
+pub struct KeyPaths {
+    /// Path to a public key.
+    pub public_key_path: String,
+
+    /// Path to a private key.
+    pub private_key_path: String,
 }
 
 /// Main struct for this library. Wraps an `oauth2::Oauth2`.
@@ -50,20 +68,20 @@ impl Oauth2Simple {
     /// will be created when calling `init()`. `refresh()` should only be called if `tokens_path`
     /// already exists or after `init()` is called.
     pub fn new(client_info_path: &Path, tokens_path: &Path, scope: &str) -> anyhow::Result<Self> {
-        let cfg = Cfg::builder()
-            .client_info_path(
-                client_info_path
-                    .to_str()
-                    .ok_or(anyhow::anyhow!("client_info_path is invalid"))?,
-            )
-            .tokens_path(
-                tokens_path
-                    .to_str()
-                    .ok_or(anyhow::anyhow!("tokens_path is invalid"))?,
-            )
-            .scope(scope)
-            .build();
-        Self::with_cfg(cfg)
+        Self::with_cfg(Self::new_basic_cfg_builder(client_info_path, tokens_path, scope)?.build())
+    }
+
+    /// Creates a new instance, as in `init()`, but includes cryptographic key data used to sign
+    /// DPoPs.
+    pub fn new_with_dpop(
+        client_info_path: &Path,
+        tokens_path: &Path,
+        scope: &str,
+        key_paths: &KeyPaths,
+    ) -> anyhow::Result<Self> {
+        let builder = Self::new_basic_cfg_builder(client_info_path, tokens_path, scope)?;
+        let builder = builder.key_paths(key_paths.clone());
+        Self::with_cfg(builder.build())
     }
 
     /// Make a new instance with the given `Cfg`.
@@ -73,15 +91,19 @@ impl Oauth2Simple {
 
         // `cfg.tokens_path` may or may not exist yet. If it does, use its values, otherwise use
         // `None`.
-        let (refresh_token, tokens) = if let Ok(e) = exists(cfg.tokens_path)
-            && e
-        {
+        let (refresh_token, dpop_nonce, tokens) = if exists(cfg.tokens_path)? {
             let tokens = read_to_string(cfg.tokens_path)?;
             let tokens: Tokens = serde_json::from_str(&tokens)?;
-            (Some(tokens.refresh_token.clone()), Some(tokens))
+            (
+                Some(tokens.refresh_token.clone()),
+                tokens.dpop_nonce.clone(),
+                Some(tokens),
+            )
         } else {
-            (None, None)
+            (None, None, None)
         };
+
+        let key_data = maybe_set_up_keys(cfg.key_paths.as_ref())?;
 
         Ok(Self {
             oauth2: Oauth2::new(oauth2::Cfg {
@@ -92,8 +114,8 @@ impl Oauth2Simple {
                 token_server_url: client_info.installed.token_uri,
                 client_secret: client_info.installed.client_secret,
                 refresh_token,
-                key_data: None,
-                dpop_nonce: None,
+                key_data,
+                dpop_nonce,
             }),
             tokens_path: cfg.tokens_path.to_string(),
             tokens,
@@ -136,7 +158,23 @@ impl Oauth2Simple {
     pub async fn refresh(&mut self) -> anyhow::Result<Tokens> {
         // Get a new access token from a refresh token.
         let resp = self.oauth2.refresh().await?;
-        let new_token_result = self.oauth2.refresh_handle_response(resp).await?;
+        let new_token_result = self.oauth2.refresh_handle_response(resp).await;
+
+        // DPoP nonces are short-lived, so we have to explicitly handle the possibility that our nonce
+        // is expired. In such an event, the server will return a `use_dpop_nonce` error code with a
+        // new nonce in a header, and then we can retry the request with the new nonce.
+        let new_token_result =
+            if let Err(RefreshHandleResponseError::UseDpopNonce { dpop_nonce, .. }) =
+                new_token_result
+            {
+                // Retry with the new nonce.
+                self.oauth2.set_dpop_nonce(&dpop_nonce);
+
+                let resp = self.oauth2.refresh().await?;
+                self.oauth2.refresh_handle_response(resp).await?
+            } else {
+                new_token_result?
+            };
 
         // Write the new data back into `self.tokens`
         let tokens = self
@@ -191,22 +229,112 @@ impl Oauth2Simple {
         self.refresh_if_needed().await
     }
 
+    /// Creates a `CfgBuilder` whose `client_info_path`, `tokens_path`, and `scope` are already
+    /// set.
+    fn new_basic_cfg_builder<'a>(
+        client_info_path: &'a Path,
+        tokens_path: &'a Path,
+        scope: &'a str,
+    ) -> anyhow::Result<
+        CfgBuilder<
+            'a,
+            cfg_builder::SetScope<cfg_builder::SetTokensPath<cfg_builder::SetClientInfoPath>>,
+        >,
+    > {
+        Ok(Cfg::builder()
+            .client_info_path(
+                client_info_path
+                    .to_str()
+                    .ok_or(anyhow::anyhow!("client_info_path is invalid"))?,
+            )
+            .tokens_path(
+                tokens_path
+                    .to_str()
+                    .ok_or(anyhow::anyhow!("tokens_path is invalid"))?,
+            )
+            .scope(scope))
+    }
+
     /// Write a `Tokens` struct out to JSON with correct permissions (on *nix systems).
     fn write_tokens(&self, tokens: &Tokens) -> anyhow::Result<()> {
         let js = serde_json::to_string_pretty(&tokens)?;
-
-        let mut f = File::create(&self.tokens_path)?;
-
-        #[cfg(unix)]
-        {
-            let md = f.metadata()?;
-            let mut perms = md.permissions();
-            perms.set_mode(0o600);
-            f.set_permissions(perms)?;
-        }
-
-        f.write_all(&js.into_bytes())?;
-
-        Ok(())
+        write_file_private(&self.tokens_path, &js.into_bytes())
     }
+}
+
+/// Set up `KeyData` if `paths` is `Some`, otherwise return `None`. If `paths` points to files that
+/// don't exist yet, keys will be generated and saved there.
+fn maybe_set_up_keys(paths: Option<&KeyPaths>) -> anyhow::Result<Option<KeyData>> {
+    Ok(if let Some(paths) = paths {
+        Some(set_up_keys(paths)?)
+    } else {
+        None
+    })
+}
+
+/// Set up `KeyData`. If `paths` points to files that don't exist yet, keys will be generated and
+/// saved there.
+fn set_up_keys(paths: &KeyPaths) -> anyhow::Result<KeyData> {
+    match (
+        exists(&paths.public_key_path)?,
+        exists(&paths.private_key_path)?,
+    ) {
+        (false, false) => generate_keys(paths),
+        (true, true) => read_keys(paths),
+        _ => Err(anyhow::anyhow!(
+            "only one of `public_key_path` and `private_key_path` exists. Either both or neither must exist"
+        )),
+    }
+}
+
+/// Generate a keypair for DPoP signing, save the keys in `paths`, and return the relevant
+/// `KeyData`.
+fn generate_keys(paths: &KeyPaths) -> anyhow::Result<KeyData> {
+    let keypair = Keypair::generate()?;
+    let keypair_pem_pkcs8: KeypairPemPkcs8 = (&keypair)
+        .try_into()
+        .map_err(|e| anyhow::anyhow!("failed to convert keypair into PEM PKCS#8: {:?}", e))?;
+
+    write_file_private(
+        &paths.public_key_path,
+        &keypair_pem_pkcs8.public_key.into_bytes(),
+    )?;
+    write_file_private(
+        &paths.private_key_path,
+        &keypair_pem_pkcs8.private_key.to_string().into_bytes(),
+    )?;
+
+    Ok((&keypair).into())
+}
+
+/// Write a file, and, if on *nix, set the permission to 0600.
+fn write_file_private(path: &str, contents: &[u8]) -> anyhow::Result<()> {
+    let mut f = File::create(path)?;
+
+    #[cfg(unix)]
+    {
+        let md = f.metadata()?;
+        let mut perms = md.permissions();
+        perms.set_mode(0o600);
+        f.set_permissions(perms)?;
+    }
+
+    f.write_all(contents)?;
+
+    Ok(())
+}
+
+/// Read keys from `paths` and extract the relevant `KeyData`. Assumes that files already exist at
+/// `paths` (won't crash if they don't exist, but also won't generate a new keypair).
+fn read_keys(paths: &KeyPaths) -> anyhow::Result<KeyData> {
+    let public_key_pem_pkcs8 = read_to_string(&paths.public_key_path)?;
+    let private_key_pem_pkcs8 = read_to_string(&paths.private_key_path)?;
+
+    let keypair_pem_pkcs8 =
+        KeypairPemPkcs8::new(public_key_pem_pkcs8, private_key_pem_pkcs8.into());
+    let keypair: Keypair = (&keypair_pem_pkcs8)
+        .try_into()
+        .map_err(|e| anyhow::anyhow!("failed to convert KeypairPemPkcs8 into Keypair: {:?}", e))?;
+
+    Ok((&keypair).into())
 }
